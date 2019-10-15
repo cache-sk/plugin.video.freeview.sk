8ce5576adf73255768c2b01241cb8140a97f892d
cache-sk <cache.sk@gmail.com>
2019-10-15 11:42:17 +0200

    work in progress

--- main.py
+++ main.py
@@ -27,15 +27,19 @@ def router(paramstring):
         module = import_module(provider)
         module.play(_handle, _addon, params)
     else:
+        xbmcplugin.endOfDirectory(_handle)
         xbmcgui.Dialog().textviewer(_addon.getAddonInfo('name'), _addon.getLocalizedString(30999))
         destination = xbmcgui.Dialog().browseSingle(3, _addon.getAddonInfo('name'), '')
-        if destination is not None:
+        if destination is not None and destination:
             try:
+                if xbmcvfs.exists(destination + 'playlist.m3u'):
+                    if not xbmcgui.Dialog().yesno(_addon.getAddonInfo('name'), _addon.getLocalizedString(30902)):
+                        return
                 xbmcvfs.copy('special://home/addons/plugin.video.freeview.sk/playlist.m3u', destination + 'playlist.m3u')
                 xbmcgui.Dialog().ok(_addon.getAddonInfo('name'), _addon.getLocalizedString(30900))
             except Exception as e:
                 xbmcgui.Dialog().ok(_addon.getAddonInfo('name'), _addon.getLocalizedString(30901), str(e))
-        xbmcplugin.endOfDirectory(_handle)
+        
 
 if __name__ == '__main__':
     router(sys.argv[2][1:])
