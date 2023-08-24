a1b75677dd39c2945ec7fe71b96f70001f41b4d8
cache-sk <cache.sk@gmail.com>
2023-08-24 10:33:20 +0200

    1.0.0 - ct, lounge

--- resources/providers/ct.py
+++ resources/providers/ct.py
@@ -12,66 +12,63 @@ import xml.etree.ElementTree as ET
 import datetime
 
 
+API = 'https://api.ceskatelevize.cz/video/v1/playlist-live/v1/stream-data/channel/'
+PARAMS = { 'canPlayDrm':'false', 'streamType':'hls', 'quality':'web', 'maxQualityCount':'5' }
 
 CHANNELS = {
-    'ct1':'CT1',
-    'ct2':'CT2',
-    'ct24':'CT24',
-    'ctsport':'CT4',
-    'ctd':'CT5',
-    'ctart':'CT6',
+    'ct1':'CH_1',
+    'ct2':'CH_2',
+    'ct24':'CH_24',
+    'ctsport':'CH_4',
+    'ctd':'CH_5',
+    'ctart':'CH_6',
     'ctdart':[{'channel':'ctd','from':8,'to':20},
               {'channel':'ctart','from':0,'to':8},
               {'channel':'ctart','from':20,'to':24}]
 }
 
-HEADERS={"Content-type": "application/x-www-form-urlencoded",
-         "Accept-encoding": "gzip",
-         "Connection": "Keep-Alive",
-         "User-Agent": "Dalvik/1.6.0 (Linux; U; Android 4.4.4; Nexus 7 Build/KTU84P)"}
+SPAPI = 'https://api.ceskatelevize.cz/graphql/'
+SPPARAMS = { 'client':'website', 'version':'1.64.1', 'operationName':'LiveBroadcastFind', 'variables':'{}', 'extensions':'{"persistedQuery":{"version":1,"sha256Hash":"cd7619a5186ef6277c1e82179c669e02e3edac97739593bc28fa32df5041d644"}}' }
+SPNONE = 'CH_7'
+SPCHANNEL = 'ctSportExtra'
+SPPREFIX = 'CH_'
 
-BASE = "https://www.ceskatelevize.cz"
+HEADERS={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'}
 
-TOKENURL = "/services/ivysilani/xml/token/"
-PLAYLISTURL = "/services/ivysilani/xml/playlisturl/"
-
-_session = requests.Session()
-_session.headers.update(HEADERS)
-
-def _https_ceska_televize_fetch(url, params):
-    response = _session.post(BASE + url, data=params)
-    return response
-
-_token = None
-
-def _token_refresh():
-    params = {"user": "iDevicesMotion"}
-    response = _https_ceska_televize_fetch(TOKENURL, params)
-    global _token
-    _token = ET.fromstring(response.content).text
-
-def _fetch(url, params):
-    if _token is None:
-        _token_refresh()
-    params["token"] = _token
-    response = _https_ceska_televize_fetch(url, params)
-    try:
-        root = ET.fromstring(response.content)
-    except:
-        return None
-    if root.tag == "errors":
-        if root[0].text == "no token sent" or root[0].text == "wrong token":
-            _token_refresh()
-            response = _https_ceska_televize_fetch(url, params)
+def get_sport_plus_channel(_addon, session):
+    response = session.get(SPAPI, params=SPPARAMS)
+    data = response.json()
+    available = []
+    titles = []
+    if 'data' in data and data['data'] and 'liveBroadcastFind' in data['data'] and data['data']['liveBroadcastFind']:
+        for live in data['data']['liveBroadcastFind']:
+            if 'id' in live and live['id'] and 'current' in live and live['current'] and 'channel' in live['current'] and live['current']['channel'] and live['current']['channel'] == SPCHANNEL:
+                title = SPCHANNEL
+                if 'title' in live['current'] and live['current']['title']:
+                    title = live['current']['title']
+                available.append(live['id'])
+                titles.append(title)
+    if len(available) == 1:
+        return SPPREFIX + available[0]
+    if len(available) > 1:
+        index = xbmcgui.Dialog().select(_addon.getAddonInfo('name'), titles)
+        if index == -1:
+            pass
         else:
-            raise Exception(', '.join([e.text for e in root]))
-    return response
+            return SPPREFIX + available[index]
+    return SPNONE
 
 def play(_handle, _addon, params):
+    session = requests.Session()
+    session.headers.update(HEADERS)
+
     channel = params['channel']
-    if not channel in CHANNELS:
+    if channel in CHANNELS:
+        channel = CHANNELS[channel]
+    elif 'ctsportplus' == channel:
+        channel = get_sport_plus_channel(_addon, session)
+    else:
         raise #TODO
-    channel = CHANNELS[channel]
     
     if isinstance(channel,list):
         now = datetime.datetime.now().hour
@@ -81,20 +78,14 @@ def play(_handle, _addon, params):
                 break
     if isinstance(channel,list):
         raise 'still list?'
-
-    response = _fetch(PLAYLISTURL, {'quality': 'web', 'ID': channel, 'playerType': 'iPad'})
-    root = ET.fromstring(response.content)
-    if root.tag == "errors":
-        raise Exception(', '.join([e.text for e in root]))
-    playlist_url = root.text
-    response = _session.get(playlist_url)
-    playlist_data = response.content
-    root = ET.fromstring(playlist_data)
-    videos = root.findall("smilRoot/body//video")
-    manifest = videos[0].get('src')
-
-    li = xbmcgui.ListItem(path=manifest)
-    li.setProperty('inputstreamaddon','inputstream.adaptive') #kodi 18
-    li.setProperty('inputstream','inputstream.adaptive') #kodi 19
-    li.setProperty('inputstream.adaptive.manifest_type','hls')
-    xbmcplugin.setResolvedUrl(_handle, True, li)
+    
+    response = session.get(API + channel, params=PARAMS)
+    data = response.json()
+    if 'streamUrls' in data and data['streamUrls'] and 'main' in data['streamUrls'] and data['streamUrls']['main']:
+        li = xbmcgui.ListItem(path=data['streamUrls']['main'])
+        li.setProperty('inputstreamaddon','inputstream.adaptive') #kodi 18
+        li.setProperty('inputstream','inputstream.adaptive') #kodi 19
+        li.setProperty('inputstream.adaptive.manifest_type','hls')
+        xbmcplugin.setResolvedUrl(_handle, True, li)
+    else:
+        raise #TODO
---
+++ resources/providers/lounge.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+# Author: cache-sk
+# Created on: 23.8.2023
+# License: AGPL v.3 https://www.gnu.org/licenses/agpl-3.0.html
+
+import xbmc
+import xbmcgui
+import xbmcplugin
+import xbmcaddon
+import requests.cookies
+import re
+import random
+import base64
+import json
+import time
+
+try:
+    from urllib import urlencode, quote_plus, quote
+except ImportError:
+    from urllib.parse import urlencode, quote_plus, quote
+
+BASE = 'https://loungetv.cz/ltv-play'
+
+HEADERS={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36','referer':'https://loungetv.cz','origin':'https://loungetv.cz'}
+
+def play(_handle, _addon, params):
+    #check kodi version
+    version = xbmc.getInfoLabel('System.BuildVersion')
+    matches = re.search('(\d+)*', version)
+    version = int(matches.group(1))
+    if version < 20:
+        xbmcgui.Dialog().ok(_addon.getAddonInfo('name'),"potrebuje 20+")
+        raise
+
+    #check inputstream helper
+    try:
+        xbmcaddon.Addon('script.module.inputstreamhelper')
+    except:
+        xbmcgui.Dialog().ok(_addon.getAddonInfo('name'),"potrebuje potrebuje script.module.inputstreamhelper")
+        raise 
+
+    timestamp = time.time()
+    session = requests.Session()
+    #session.verify = False
+    headers = {}
+    headers.update(HEADERS)
+    response = session.get(BASE, headers=headers)
+    content = response.content
+    try:
+        content = content.decode('utf-8')
+    except AttributeError:
+        pass
+        
+    matches = re.search('xmlHttp\.open\(\'POST\', \'([^\s]+)\', false\);', content)
+    nextstep = matches.group(1)
+
+    matches = re.search('var customers_token = \'(.+)\';', content)
+    customers_token = matches.group(1)
+    customers_token = customers_token.strip()
+    headers.update({'Authorization':'Bearer '+customers_token})
+    
+    matches = re.search('var profiles_id = \'(.+)\';', content)
+    profiles_id = matches.group(1)
+    headers.update({'profilesId':base64.b64encode(profiles_id.encode('utf-8'))})
+    
+    matches = re.search('var devices_type = \'(.+)\';', content)
+    devices_type = matches.group(1)
+    headers.update({'devicesType':base64.b64encode(devices_type.encode('utf-8'))})
+    
+    matches = re.search('var offset = ([0-9]+);', content)
+    offset = int(matches.group(1))
+
+    matches = re.search('var version = \'(.+)\';', content)
+    version = matches.group(1)
+    
+    matches = re.search('var edges_id = ([0-9]+);', content)
+    edges_id = int(matches.group(1))
+    
+    matches = re.search('var devices_identification = \'(.+)\';', content)
+    devices_identification = matches.group(1)
+    
+    matches = re.search('var devices_hash = \'(.+)\';', content)
+    devices_hash = matches.group(1)
+    
+    matches = re.search('\'com.widevine.alpha\': \'(.+)\',', content)
+    widevine = matches.group(1)
+    
+    matches = re.search('xmlHttp\.send\(\'(.+)\'\);', content)
+    data = matches.group(1)
+    
+    response = session.post(nextstep, data, headers=headers)
+    stream = response.content
+    try:
+        stream = stream.decode('utf-8')
+    except AttributeError:
+        pass
+    stream = json.loads(stream)
+    
+    uheaders = urlencode(headers)
+    data_drm = {}
+    data_drm['timestamp'] = int(timestamp)
+    data_drm['offset'] = offset
+    data_drm['version'] = version
+    data_drm['version'] = version
+    data_drm['edges_id'] = edges_id
+    data_drm['devices_identification'] = devices_identification
+    data_drm['devices_hash'] = devices_hash
+    data_drm['rawLicense'] = 'b{SSM}'
+    drm_license = widevine + '|' + uheaders + '|' + json.dumps(data_drm) + '|JBrawLicense'
+
+    li = xbmcgui.ListItem(path=stream['response']['url']+'|'+uheaders)
+    li.setProperty('inputstream','inputstream.adaptive')
+    li.setProperty('inputstream.adaptive.stream_headers',uheaders)
+    li.setProperty('inputstream.adaptive.manifest_type','mpd')
+    li.setProperty('inputstream.adaptive.license_type','com.widevine.alpha')
+    li.setProperty('inputstream.adaptive.license_key',drm_license)
+    xbmcplugin.setResolvedUrl(_handle, True, li)
